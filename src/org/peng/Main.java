b9fb1182c5b248f1082aeb16ef525ad550ea8cbf
pengeman <peng4567@163.com>
2022-06-21 16:57:13 +0800

    重写示例代码.

--- src/org/peng/Main.java
+++ src/org/peng/Main.java
@@ -1,15 +1,36 @@
 package org.peng;
 
+import javax.activation.MailcapCommandMap;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("this is main...");
-        URL url = Main.class.getResource("/ds1.properties");
-        System.out.println("1 url : " + url);
-        url = ClassLoader.getSystemResource("ds1.properties");
-        System.out.println("2 url : " + url);
+        Main lf = new Main();
+        lf.earth();
+    }
+
+    /**
+     * java 被设置成source(源码)目录,编译部署后，就是程序的跟目录
+     * resource被设置成resource(资源)，编译部署后，被放置在跟下.不论开发模式下resource在什么位置
+     * 读取resource中的文件，使用Class.getResourceAsStream("/xxx"),就是从跟下寻找并读取文件 。
+     * 是所以classLoader读取文件，是从程序的当前目录中寻找并读取。
+     */
+    private void earth(){
+
+        java.io.InputStream fis = this.getClass().getResourceAsStream("/it.properties");
+        Properties pro = new Properties();
+        try {
+            pro.load(fis);
+            String me = pro.getProperty("me");
+            String you = pro.getProperty("you");
+            String he = pro.getProperty("he");
+            System.out.println(me + you + he);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
